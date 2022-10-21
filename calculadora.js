98aa8de23277a27327572510d45db24fea574e1b
Defelippekevin <kevin.defelippe@gmail.com>
2022-10-21 16:40:32 -0300

    agregada vinculacion entre js y html

--- calculadora.js
+++ calculadora.js
@@ -1,6 +1,6 @@
 "use strict"
 
-let operando1;
-let operador;
-let operando2;   
+let operando1=document.querySelector("#operando1").value;
+let operador=document.querySelector("#operando").value;
+let operando2=document.querySelector("#operando2").value;
 let resultado;
